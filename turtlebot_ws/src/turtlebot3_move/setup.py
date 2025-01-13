b3fa26ad38a0b0adbb2e3f6478facdc190bc362c
Hyuna Kim <sjajmh6612@naver.com>
2025-01-13 21:23:58 +0900

    yolo&system monitor source code

---
+++ turtlebot_ws/src/turtlebot3_move/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot3_move'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hyuna',
+    maintainer_email='sjajmh6612@naver.com',
+    description='A ROS2 package for controlling TurtleBot3 movements',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [ 
+        'turtleview = turtlebot3_move.detect_and_send:main',
+        'rviz_capture = turtlebot3_move.rviz_capture_and_send:main'
+        ],
+    },
+)
